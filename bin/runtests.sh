ae959cfaf05698a2a9cb7437c0422e5c7e85e581
Joe Schneider <joeschneider@invenshure.com>
2015-12-14 12:05:52 -0600

    Testing infra updates

--- bin/runtests.sh
+++ bin/runtests.sh
@@ -3,15 +3,15 @@
 unit_test_path=test/unit_tests/
 code_path=api/
 
+cd "$( dirname "${BASH_SOURCE[0]}" )/.."
+
 (
 case "$1-$2" in
   unit-|unit---ci)
-    cd `git rev-parse --show-toplevel`
     PYTHONPATH=. py.test --cov=api --cov-report=term-missing $unit_test_path
     ;;
   unit---watch)
-    cd `git rev-parse --show-toplevel`
-    PYTHONPATH=. ptw $unit_test_path $code_path -- $unit_test_path
+    PYTHONPATH=. ptw $unit_test_path $code_path --poll -- $unit_test_path
     ;;
   integration-|integration---ci|integration---watch)
     echo "Not implemented yet"
