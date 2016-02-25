5f0f5ecca90539b663d988119c83dbcb7eac4f56
Ryan Sanford <ryansanford@flywheel.io>
2016-02-25 08:06:33 -0600

    Remove branch reference
    Don't reference branches from within source. This allows core and testdata repo to move without breaking not up-to-date versions of core. Also allows branches to be fully merged.
    
    Still a similar branch reference in bin/run.sh

--- docker/bootstrap-data.sh
+++ docker/bootstrap-data.sh
@@ -15,7 +15,13 @@ GET_LATEST_DATA=${1:-N}
 
 # Hard code some other vars important for bootstrapping
 #
-bootstrap_data_branch=master
+
+# Set the commit hash or tag or branch desired for scitran/testdata.
+# Branch name should only be used for testing convenience.
+#
+# When changing scitran/testdata, merge that change to master first,
+# then reference that resulting commit hash here.
+bootstrap_data_label=45056c104caf85796e6138a5ca163c3937c7b5d9
 
 
 # Move to API folder for relative path assumptions later on
@@ -30,7 +36,7 @@ export PYTHONPATH=.
 # Bootstrap data
 
 # Compare hash of source test data to most recent download. Remove local copy to force re-download if they are different.
-TESTDATA_URL="https://github.com/scitran/testdata/archive/${bootstrap_data_branch}.tar.gz"
+TESTDATA_URL="https://github.com/scitran/testdata/archive/${bootstrap_data_label}.tar.gz"
 TESTDATA_VERSION=$(curl -sLI ${TESTDATA_URL} | grep ETag | tail -n 1 | cut -f 2 -d '"')
 
 # use hidden
