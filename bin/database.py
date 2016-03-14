14e86919281d85ae62875260aea805bc70ecaf3a
Ryan Sanford <ryansanford@flywheel.io>
2016-03-14 13:48:32 -0500

    Bug fixes

--- bin/database.py
+++ bin/database.py
@@ -1,13 +1,13 @@
+#!/usr/bin/env python
 import json
 
-from .. import config
+from api import config
 
 
 print 'This is the stub database maintenance script.'
 
-print 'DB uri: ' + str(config.get_item('persistent', 'db_uri')))
+print 'DB uri: ' + str(config.get_item('persistent', 'db_uri'))
 
 user = config.db.users.find_one({})
-user_j = json.dumps(user)
 
-print 'Example user from db: ' + user_j
+print 'Example user from db: {0}'.format(user)
