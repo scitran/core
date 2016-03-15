a7fc5c2310c74c26054510b8a531cc435a2bb183
nagem <meganhenning@flywheel.io>
2016-03-15 16:42:02 -0500

    Fix missing param in container auth validator

--- api/auth/containerauth.py
+++ api/auth/containerauth.py
@@ -15,7 +15,7 @@ def default_container(handler, container=None, target_parent_container=None):
     on the container before actually executing this method.
     """
     def g(exec_op):
-        def f(method, _id=None, payload=None):
+        def f(method, _id=None, payload=None, recursive=False):
             if method == 'GET' and container.get('public', False):
                 has_access = True
             elif method == 'GET':
