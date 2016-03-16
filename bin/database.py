4bdc3a9b1d5d53c25a0bd0dd51a1985c18b8e349
nagem <meganhenning@flywheel.io>
2016-03-16 14:04:12 -0500

    Flesh out potential db version check

--- bin/database.py
+++ bin/database.py
@@ -1,29 +1,48 @@
 #!/usr/bin/env python
 
-"""
-Suggested parameters:
-    upgradeSchema       Upgrades the DB to the required schema version.
-                        Returns (0) if upgrade is successful
-
-    confirmSchemaMatch  Returns (0)  if DB schema version matches requirements.
-                        Returns (42) if DB schema version does not match
-                                     requirements and can be upgraded.
-                        Returns (43) if DB schema version does not match
-                                     requirements and cannot be upgraded,
-                                     perhaps because code is at lower version
-                                     than the DB schema version.
-"""
-
-
 import json
-
 from api import config
 
+CURRENT_DATABASE_VERSION = 1 # An int that is bumped when a new 
 
-print 'This is the stub database maintenance script.'
+def confirm_schema_match():
+	"""
+	Checks version of database schema
 
-print 'DB uri: ' + str(config.get_item('persistent', 'db_uri'))
+	Returns (0)  if DB schema version matches requirements.
+	Returns (42) if DB schema version does not match
+	             requirements and can be upgraded.
+	Returns (43) if DB schema version does not match
+	             requirements and cannot be upgraded,
+	             perhaps because code is at lower version
+	             than the DB schema version.
+	"""
 
-user = config.db.users.find_one({})
+	version = config.db.version.find_one({"_id": "version"})
+	if version is None or version.get('database', None) is None:
+		return 42 # At version 0
 
-print 'Example user from db: {0}'.format(user)
+	db_version = version.get('database', 0)
+	if not isinstance(db_version, int) or db_version > CURRENT_DATABASE_VERSION:
+		return 43 
+	elif db_version < CURRENT_DATABASE_VERSION:
+		return 42
+	else:
+		return 0
+
+def upgrade_schema():
+	"""
+	Upgrades db to the current schema version
+    
+    Returns (0) if upgrade is successful
+    """
+
+	# In progress
+	# db_version = version.get('database',0)
+	
+	# if db_version < 1:
+	# 	# rename the metadata fields
+	# 	config.db.container.update_many({}, {"$rename": {"metadata": "info"}})
+
+	# config.db.version.update_one({"_id": "version"}, {"$set": {"database": CURRENT_DATABASE_VERSION}})
+	return 0
