5d054c2468bcc273a2c62be582c99e8ccd1c3c8f
Ryan Sanford <ryansanford@flywheel.io>
2016-03-16 10:03:46 -0500

    Document downgrade condition

--- bin/database.py
+++ bin/database.py
@@ -5,8 +5,13 @@ Suggested parameters:
     upgradeSchema       Upgrades the DB to the required schema version.
                         Returns (0) if upgrade is successful
 
-    confirmSchemaMatch  Returns (0) if DB schema version matches requirements.
-                        Returns (42) if DB schema version does not match requirements.
+    confirmSchemaMatch  Returns (0)  if DB schema version matches requirements.
+                        Returns (42) if DB schema version does not match
+                                     requirements and can be upgraded.
+                        Returns (43) if DB schema version does not match
+                                     requirements and cannot be upgraded,
+                                     perhaps because code is at lower version
+                                     than the DB schema version.
 """
 
 
