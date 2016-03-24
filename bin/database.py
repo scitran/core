77e8aa133f4b2fc3dca9cb5a24a903484646e707
nagem <meganhenning@flywheel.io>
2016-03-24 17:46:25 -0500

    Use logging package

--- bin/database.py
+++ bin/database.py
@@ -2,6 +2,7 @@
 
 import json
 import sys
+import logging
 from api import config
 
 CURRENT_DATABASE_VERSION = 1 # An int that is bumped when a new schema change is made
@@ -30,11 +31,17 @@ def confirm_schema_match():
 
     db_version = get_db_version()
     if not isinstance(db_version, int) or db_version > CURRENT_DATABASE_VERSION:
+        logging.error('The stored db schema version of %s is incompatible with required version %s', 
+                       str(db_version), CURRENT_DATABASE_VERSION)
         sys.exit(43) 
     elif db_version < CURRENT_DATABASE_VERSION:
         sys.exit(42)
     else:
         sys.exit(0)
+
+def upgrade_to_1():
+    # scitran/core issue #206
+    config.db.version.insert_one({'_id': 'version', 'database': CURRENT_DATABASE_VERSION})
 
 def upgrade_schema():
     """
@@ -46,15 +53,14 @@ def upgrade_schema():
     db_version = get_db_version()
     try:
         if db_version < 1:
-            # scitran/core issue #206
-            config.db.version.insert_one({'_id': 'version', 'database': CURRENT_DATABASE_VERSION})
+            upgrade_to_1()
     except Exception as e:
-        print 'Incremental upgrade of db failed'
-        print e
+        logging.exception('Incremental upgrade of db failed')
         sys.exit(1)
     else:
         config.db.version.update_one({'_id': 'version'}, {'$set': {'database': CURRENT_DATABASE_VERSION}})
         sys.exit(0)
+
 try:
     if len(sys.argv) > 1:
         if sys.argv[1] == 'confirm_schema_match':
@@ -62,11 +68,11 @@ try:
         elif sys.argv[1] == 'upgrade_schema':
             upgrade_schema()
         else:
-            print 'Unknown method name given as argv to database.py'
+            logging.error('Unknown method name given as argv to database.py')
             sys.exit(1)
     else:
-        print 'No method name given as argv to database.py'
+        logging.error('No method name given as argv to database.py')
         sys.exit(1)
 except Exception as e:
-    print e
+    logging.exception()
     sys.exit(1)
