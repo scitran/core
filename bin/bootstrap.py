73ca5c50c016d66e8d1ea21db157241726a0c92f
Ryan Sanford <ryansanford@flywheel.io>
2016-05-17 10:49:14 -0500

    bootstrap exit 1 if http requests fail

--- bin/bootstrap.py
+++ bin/bootstrap.py
@@ -20,7 +20,37 @@ log = logging.getLogger('scitran.bootstrap')
 logging.getLogger('requests').setLevel(logging.WARNING) # silence Requests library
 
 
+def _upsert_user(request_session, api_url, user_doc):
+    """
+    Insert or update user and return requests.Response object.
+
+    user_doc: valid user doc defined in user input schema.
+    """
+    get_user_resp = request_session.get(api_url + '/users/' + user_doc['_id'])
+    if get_user_resp.ok:
+        return request_session.put(api_url + '/users/' + user_doc['_id'], json=user_doc)
+    else:
+        return request_session.post(api_url + '/users', json=user_doc)
+
+def _upsert_role(request_session, api_url, role_doc, group_id):
+    """
+    Insert or update group role and return requests.Response object.
+
+    role_doc: valid permission doc defined in permission input schema.
+    """
+    full_role_url = "{0}/groups/{1}/roles/{2}/{3}".format(api_url, group_id, role_doc['site'], role_doc['_id'])
+    get_role_resp = request_session.get(full_role_url)
+    if get_role_resp.ok:
+        return request_session.put(full_role_url, json=role_doc)
+    else:
+        return request_session.post("{0}/groups/{1}/roles".format(api_url, group_id) , json=role_doc)
+
 def users(filepath, api_url, http_headers, insecure):
+    """
+    Upserts the users/groups/roles defined in filepath parameter.
+
+    Throws requests.HTTPError if upserts fail.
+    """
     now = datetime.datetime.utcnow()
     with open(filepath) as fd:
         input_data = json.load(fd)
@@ -29,26 +59,24 @@ def users(filepath, api_url, http_headers, insecure):
         rs.verify = not insecure
         rs.headers = http_headers
         for u in input_data.get('users', []):
-            r = rs.post(api_url + '/users', json=u)
-            if r.ok:
-                log.info('S     ' + u['_id'])
-            else:
-                log.error('F     ' + u['_id'])
+            log.info('    {0}'.format(u['_id']))
+            r = _upsert_user(request_session=rs, api_url=api_url, user_doc=u)
+            r.raise_for_status()
 
         log.info('bootstrapping groups...')
-        site_id = rs.get(api_url + '/config').json()['site']['id']
+        r = rs.get(api_url + '/config')
+        r.raise_for_status()
+        site_id = r.json()['site']['id']
         for g in input_data.get('groups', []):
             roles = g.pop('roles')
+            log.info('    {0}'.format(g['_id']))
             r = rs.post(api_url + '/groups' , json=g)
-            if r.ok:
-                log.info('S     ' + g['_id'])
-            else:
-                log.error('F     ' + g['_id'])
+            r.raise_for_status()
             for role in roles:
                 role.setdefault('site', site_id)
-                r = rs.post(api_url + '/groups/' + g['_id'] + '/roles' , json=role)
-                if not r.ok:
-                    log.error('F     ' + str(role) + ' -> ' + g['_id'])
+
+                r = _upsert_role(request_session=rs, api_url=api_url, role_doc=role, group_id=g['_id'])
+                r.raise_for_status()
     log.info('bootstrapping complete')
 
 
@@ -71,4 +99,13 @@ if args.secret:
     http_headers['X-SciTran-Auth'] = args.secret
 # TODO: extend this to support oauth tokens
 
-users(args.json, args.url, http_headers, args.insecure)
+try:
+    users(args.json, args.url, http_headers, args.insecure)
+except requests.HTTPError as http_ex:
+    log.error(http_ex)
+    log.error("request_body={0}".format(http_ex.response.request.body))
+    sys.exit(1)
+except Exception as ex:
+    log.error('Unexpected error:')
+    log.error(ex)
+    sys.exit(1)
