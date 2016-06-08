54efa94018cd13c61c93c3c9d72471e50d0cff1e
Ryan Sanford <ryansanford@flywheel.io>
2016-06-08 07:54:22 -0500

    test execution script fail on bootstrap error

--- bin/runtests.sh
+++ bin/runtests.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# Convenience script for unit and integration test execution consumed by
+# continous integration workflow (travis)
+#
+# Must return non-zero on any failure.
 set -e
 
 unit_test_path=test/unit_tests/
@@ -19,13 +24,24 @@ case "$1-$2" in
     PYTHONPATH=. ptw $unit_test_path $code_path --poll -- $unit_test_path
     ;;
   integration---ci|integration-)
+    # Bootstrap and run integration test.
+    #  - always stop and remove docker containers
+    #  - always exit non-zero if either bootstrap or integration tests fail.
+    #  - only run integration tests on bootstrap success
+
       docker-compose \
         -f test/docker-compose.yml \
         run \
         --rm \
         bootstrap  && \
-      docker-compose -f test/docker-compose.yml run --rm integration-test
+      docker-compose \
+        -f test/docker-compose.yml \
+        run \
+        --rm \
+        integration-test || \
+      exit_code=1
     docker-compose -f test/docker-compose.yml down
+    exit $exit_code
     ;;
   integration---watch)
     echo "Not implemented"
