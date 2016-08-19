24c2ccc2749d829459b9e9fc49236ae7215c13b9
Ryan Sanford <ryansanford@flywheel.io>
2016-08-19 08:38:55 -0500

    Remove sphinx REST API doc generation/hosting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,6 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
-    'sphinxcontrib.httpdomain',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
