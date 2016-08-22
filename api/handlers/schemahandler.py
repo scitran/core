6c40674302aa16d3002d395855b370b2f34347d1
Colton Leekley-Winslow <coltonlw@flywheel.io>
2016-08-22 13:25:58 -0500

    schema_path is no longer user configurable

--- api/handlers/schemahandler.py
+++ api/handlers/schemahandler.py
@@ -12,7 +12,7 @@ class SchemaHandler(base.RequestHandler):
         super(SchemaHandler, self).__init__(request, response)
 
     def get(self, schema):
-        schema_path = os.path.join(config.get_item('persistent', 'schema_path'), schema)
+        schema_path = os.path.join(config.schema_path, schema)
         try:
             with open(schema_path, 'rU') as f:
                 return json.load(f)
