9bfed45562234e6e21a7d49b8f93f2476de834fa
Colton Leekley-Winslow <coltonlw@flywheel.io>
2016-09-07 14:20:40 -0500

    remove __pycache__ directory

--- test/bin/run-unit-tests.sh
+++ test/bin/run-unit-tests.sh
@@ -11,4 +11,9 @@ echo "Checking for files with windows style newline:"
 ! find * -path "runtime" -prune -o -path "persistent" -prune -o -type f \
   -exec grep -rI $'\r' {} \+
 
+# Remove __pycache__ directory for issue with __file__ attribute
+# Due to running the tests on the host creating bytecode files
+# Which have a mismatched __file__ attribute when loaded in docker container
+rm -rf test/unit_tests/python/__pycache__
+
 PYTHONPATH="$( pwd )"  py.test --cov=api --cov-append test/unit_tests/python
