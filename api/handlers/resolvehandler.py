8d0eb1f9802951e63244699873c21d02b7482d03
Nathaniel Kofalt <nathaniel@kofalt.com>
2016-11-11 08:36:06 -0600

    Server-side resolver

---
+++ api/handlers/resolvehandler.py
@@ -0,0 +1,53 @@
+"""
+API request handlers for the jobs module
+"""
+
+from .. import base
+from .. import config
+from ..resolver import Resolver
+
+class ResolveHandler(base.RequestHandler):
+
+    """Provide /resolve API route."""
+
+    def resolve(self):
+        """Resolve a path through the hierarchy."""
+
+        if self.public_request:
+            self.abort(403, 'Request requires login')
+
+        doc = self.request.json
+        result = Resolver.resolve(doc['path'])
+
+        # Cancel the request if anything in the path is unauthorized; remove any children that are unauthorized.
+        if not self.superuser_request:
+            for x in result["path"]:
+                ok = False
+                if x['node_type'] in ['acquisition', 'session', 'project', 'group']:
+                    perms = x.get('roles', []) + x.get('permissions', [])
+                    for y in perms:
+                        if y.get('_id') == self.uid:
+                            ok = True
+                            break
+
+                    if not ok:
+                        self.abort(403, "Not authorized")
+
+            filtered_children = []
+            for x in result["children"]:
+                ok = False
+                if x['node_type'] in ['acquisition', 'session', 'project', 'group']:
+                    perms = x.get('roles', []) + x.get('permissions', [])
+                    for y in perms:
+                        if y.get('_id') == self.uid:
+                            ok = True
+                            break
+                else:
+                    ok = True
+
+                if ok:
+                    filtered_children.append(x)
+
+            result["children"] = filtered_children
+
+        return result
---
+++ api/resolver.py
@@ -0,0 +1,203 @@
+"""
+Resolve an ambiguous path through the data hierarchy.
+"""
+
+from . import config
+from . import util
+
+class Node(object):
+
+    # All lists obtained by the Resolver are sorted by the created timestamp, then the database ID as a fallback.
+    # As neither property should ever change, this sort should be consistent
+    sorting = [('created', 1), ('_id', 1)]
+
+    # Globally disable extraneous properties of unbounded length.
+    projection = {'files': 0}
+    # Version of same for debugging purposes.
+    # projection = {'roles': 0, 'permissions': 0, 'files': 0}
+
+    @staticmethod
+    def get_children(parent):
+        raise NotImplementedError()
+
+    @staticmethod
+    def filter(children, criterion):
+        raise NotImplementedError()
+
+
+def _pipeline(table, pipeline):
+    """
+    Temporary philosophical dupe with reporthandler.py.
+    Execute a mongo pipeline, check status, return results.
+    A workaround for wonky pymongo aggregation behavior.
+    """
+
+    output = config.db.command('aggregate', table, pipeline=pipeline)
+    result = output.get('result')
+
+    if output.get('ok') != 1.0 or result is None:
+        raise Exception()
+
+    return result
+
+def _get_files(table, match):
+    """
+    Return a consistently-ordered set of files for a given container query.
+    """
+
+    pipeline = [
+        {'$match': match },
+        {'$unwind': '$files'},
+        {'$sort': {'files.name': 1}},
+        {'$group': {'_id':'$_id', 'files': {'$push':'$files'}}}
+    ]
+
+    result = _pipeline(table, pipeline)
+    if len(result) == 0:
+        return []
+
+    files = result[0]['files']
+    for x in files:
+        x.update({'node_type': 'file'})
+    return files
+
+def _get_docs(table, label, match):
+    results = list(config.db[table].find(match, Node.projection, sort=Node.sorting))
+    for y in results:
+        y.update({'node_type': label})
+    return results
+
+
+class CollectionsLiteralNode(Node):
+    pass
+
+class FileNode(Node):
+    @staticmethod
+    def get_children(parent):
+        return []
+
+    @staticmethod
+    def filter(children, criterion):
+        raise Exception("Files have no children")
+
+class AcquisitionNode(Node):
+    @staticmethod
+    def get_children(parent):
+        files    = _get_files('acquisitions', {'_id' : parent['_id'] })
+
+        return files
+
+    @staticmethod
+    def filter(children, criterion):
+        for x in children:
+            if x['node_type'] == "file" and x.get('name') == criterion:
+                return x, FileNode
+        raise Exception('No ' + criterion + ' acquisition or file found.')
+
+class SessionNode(Node):
+
+    @staticmethod
+    def get_children(parent):
+        acqs = _get_docs('acquisitions', 'acquisition', {'session' : parent['_id']})
+        files    = _get_files('sessions', {'_id' : parent['_id'] })
+
+        return list(acqs) + files
+
+    @staticmethod
+    def filter(children, criterion):
+        for x in children:
+            if x['node_type'] == "acquisition" and x.get('label') == criterion:
+                return x, AcquisitionNode
+            if x['node_type'] == "file" and x.get('name') == criterion:
+                return x, FileNode
+        raise Exception('No ' + criterion + ' acquisition or file found.')
+
+class ProjectNode(Node):
+
+    @staticmethod
+    def get_children(parent):
+        sessions = _get_docs('sessions', 'session', {'project' : parent['_id']})
+        files    = _get_files('projects', {'_id' : parent['_id'] })
+
+        return list(sessions) + files
+
+    @staticmethod
+    def filter(children, criterion):
+        for x in children:
+            if x['node_type'] == "session" and x.get('label') == criterion:
+                return x, SessionNode
+            if x['node_type'] == "file" and x.get('name') == criterion:
+                return x, FileNode
+        raise Exception('No ' + criterion + ' session or file found.')
+
+class GroupNode(Node):
+
+    @staticmethod
+    def get_children(parent):
+        projects = _get_docs('projects', 'project', {'group' : parent['_id']})
+        return projects
+
+    @staticmethod
+    def filter(children, criterion):
+        for x in children:
+            if x.get('label') == criterion:
+                return x, ProjectNode
+        raise Exception('No ' + criterion + ' project found.')
+
+class RootNode(Node):
+
+    @staticmethod
+    def get_children(parent):
+        groups = _get_docs('groups', 'group', {})
+        return groups
+
+    @staticmethod
+    def filter(children, criterion):
+        for x in children:
+            if x.get('_id') == criterion:
+                return x, GroupNode
+        raise Exception('No ' + criterion + ' group found.')
+
+
+class Resolver(object):
+    """
+    Given an array of human-meaningful, possibly-ambiguous strings, resolve it as a path through the hierarchy.
+
+    Does not tolerate ambiguity at any level of the path except the final node.
+    """
+
+    @staticmethod
+    def resolve(path, uid=None):
+
+        if not isinstance(path, list):
+            raise Exception("Path must be an array of strings")
+
+        node, resolved, last = Resolver._resolve(path, RootNode)
+        children = node.get_children(last)
+
+        return {
+            'path': resolved,
+            'children': children
+        }
+
+    @staticmethod
+    def _resolve(path, node, parents=None):
+
+        if parents is None:
+            parents = []
+
+        last = None
+        if len(parents) > 0:
+            last = parents[len(parents) - 1]
+
+        if len(path) == 0:
+            return node, parents, last
+
+        current  = path[0]
+        children = node.get_children(last)
+        selected, next_ = node.filter(children, current)
+
+        path = path[1:]
+        parents.append(selected)
+
+        return Resolver._resolve(path, next_, parents)
