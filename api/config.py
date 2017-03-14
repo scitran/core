2556b74873af386d0ed3330374d50ec645518c33
Megan Henning <meganhenning@flywheel.io>
2017-03-14 12:56:57 -0500

    update ttl of auth_tokens to 30 days

--- api/config.py
+++ api/config.py
@@ -244,7 +244,7 @@ def initialize_db():
         upsert=True
     )
 
-    create_or_recreate_ttl_index('authtokens', 'timestamp', 604800)
+    create_or_recreate_ttl_index('authtokens', 'timestamp', 2592000)
     create_or_recreate_ttl_index('uploads', 'timestamp', 60)
     create_or_recreate_ttl_index('downloads', 'timestamp', 60)
 
