9c12630493a221c46a6ecda7d97b6ed5b31e96c1
Ambrus Simon <ambrussimon@invenshure.com>
2017-03-20 12:52:59 +0100

    fix label/uid upload tests (and extend coverage)

--- test/integration_tests/python/test_uploads.py
+++ test/integration_tests/python/test_uploads.py
@@ -14,10 +14,26 @@ log.addHandler(sh)
 def with_group_and_file_data(api_as_admin, data_builder, bunch, request):
     group_id = 'test_group_' + str(int(time.time() * 1000))
     data_builder.create_group(group_id)
-    file_names = ['proj.csv', 'ses.csv', 'acq.csv']
     files = {}
-    for i, name in enumerate(file_names):
-        files['file' + str(i+1)] = (name, 'some,data,to,send\nanother,row,to,send\n')
+    for i, cont in enumerate(['project', 'subject', 'session', 'acquisition', 'unused']):
+        files['file' + str(i+1)] = (cont + '.csv', 'some,data,to,send\nanother,row,to,send\n')
+    files['metadata'] = {
+        'group': {'_id': group_id},
+        'project': {
+            'label': 'test_project',
+            'files': [{'name': 'project.csv'}]
+        },
+        'session': {
+            'subject': {
+                'code': 'test_subject_code',
+                'files': [{'name': 'subject.csv'}]
+            },
+            'files': [{'name': 'session.csv'}]
+        },
+        'acquisition': {
+            'files': [{'name': 'acquisition.csv'}]
+        }
+    }
 
     def teardown_db():
         r = api_as_admin.get('/groups/{}/projects'.format(group_id))
@@ -40,111 +56,36 @@ def with_group_and_file_data(api_as_admin, data_builder, bunch, request):
     request.addfinalizer(teardown_db)
 
     fixture_data = bunch.create()
-    fixture_data.group_id = group_id
     fixture_data.files = files
     return fixture_data
 
 
 def test_uid_upload(with_group_and_file_data, api_as_admin):
     data = with_group_and_file_data
-    metadata = {
-        'group':{
-            '_id': data.group_id
-        },
-        'project':{
-            'label':'test_project',
-            'files':[
-                {
-                    'name':data.files.keys()[0]
-                }
-            ]
-        },
-        'session':{
-            'uid':'test_session_uid',
-            'files':[
-                {
-                    'name':data.files.keys()[1]
-                }
-            ],
-            'subject': {'code': 'test_subject'}
-        },
-        'acquisition':{
-            'uid':'test_acquisition_uid',
-            'files':[
-                {
-                    'name':data.files.keys()[2]
-                }
-            ]
-        }
-    }
-    metadata = json.dumps(metadata)
-    data.files['metadata'] = ('', metadata)
+    metadata = data.files['metadata']
+    metadata['session']['uid'] = 'test_session_uid'
+    metadata['acquisition']['uid'] = 'test_acquisition_uid'
 
+    # try to uid-upload w/o metadata
+    del data.files['metadata']
+    r = api_as_admin.post('/upload/uid', files=data.files)
+    assert r.status_code == 500
+
+    # uid-upload files
+    data.files['metadata'] = ('', json.dumps(metadata))
     r = api_as_admin.post('/upload/uid', files=data.files)
     assert r.ok
 
 
 def test_label_upload(with_group_and_file_data, api_as_admin):
     data = with_group_and_file_data
-    metadata = {
-        'group':{
-            '_id': data.group_id
-        },
-        'project':{
-            'label':'test_project',
-            'files':[
-                {
-                    'name':data.files.keys()[0]
-                }
-            ]
-        },
-        'session':{
-            'label':'test_session',
-            'files':[
-                {
-                    'name':data.files.keys()[1]
-                }
-            ],
-            'subject': {'code': 'test_subject'}
-        },
-        'acquisition':{
-            'label':'test_acquisition',
-            'files':[
-                {
-                    'name':data.files.keys()[2]
-                }
-            ]
-        }
-    }
-    metadata = json.dumps(metadata)
-    data.files['metadata'] = ('', metadata)
+    metadata = data.files['metadata']
+    metadata['session']['label'] = 'test_session_label'
+    metadata['acquisition']['label'] = 'test_acquisition_label'
 
+    # label-upload files
+    data.files['metadata'] = ('', json.dumps(metadata))
     r = api_as_admin.post('/upload/label', files=data.files)
-    assert r.ok
-    assert r.json() == []
-
-    data.files['metadata'] = metadata
-    r = api_as_admin.post('/upload/label', files=data.files)
-    assert r.status_code == 400
-
-    # NOTE this adds api/placer.py coverage for lines 177-185
-    # need info on why that wasn't covered with 1st request in this test
-    single_file_data = {
-        'file': ('acq.csv', 'some,data,to,send\nanother,row,to,send\n'),
-        'metadata': ('', json.dumps({
-            'group': {'_id': data.group_id},
-            'project': {'label': 'test_project'},
-            'session': {
-                'label': 'test_session',
-                'subject': {'code': 'test_subject'}
-            },
-            'acquisition': {
-                'label': 'test_acquisition',
-                'files': [{'name': 'acq.csv'}]
-            }
-        }))
-    }
-    r = api_as_admin.post('/upload/label', files=single_file_data)
     assert r.ok
 
 
