10bdba024601e8017184b50150ad956b420fcb7a
Ryan Sanford <ryansanford@flywheel.io>
2017-03-23 08:52:27 -0500

    inject key returns success if key is unchanged

--- test/bin/inject_api_key.py
+++ test/bin/inject_api_key.py
@@ -19,6 +19,10 @@ from api.dao.containerstorage import ContainerStorage
 def set_api_key(user_id, api_key):
     """Sets an API key for a user in the database
 
+    Overwrites existing keys and return success if existing key matches. Raises
+    exception if matching user cannot be found, or database record of matching
+    user fails to be updated.
+
     Args:
         user_id (str): ID of the user to reset the api key for
         api_key (str): api_key to set for given user
@@ -29,6 +33,13 @@ def set_api_key(user_id, api_key):
             "key":api_key
         }
     }
+    matching_user = users_storage.exec_op('GET', _id=user_id)
+    if matching_user is None:
+        raise RuntimeError("Unable to find matching user: " + user_id)
+
+    if matching_user['api_key'] == api_key_doc['api_key']:
+        return
+
     result = users_storage.exec_op('PUT', _id=user_id, payload=api_key_doc)
     if result.modified_count != 1:
         raise RuntimeError("Unable to set API key for user")
