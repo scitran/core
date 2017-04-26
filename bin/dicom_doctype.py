fbb05f3341568b58b7a6c3fcc95ad5923598fbec
Megan Henning <meganhenning@flywheel.io>
2017-04-26 14:16:27 -0500

    I didn't think it'd have to come to this

--- bin/dicom_doctype.py
+++ bin/dicom_doctype.py
@@ -360,7 +360,7 @@ if __name__ == '__main__':
 
     request = {
         'settings': {
-            "index.mapping.total_fields.limit": 2500,
+            "index.mapping.total_fields.limit": 4000,
             'number_of_shards': 1,
             'number_of_replicas': 0,
             'analysis' : ANALYSIS
