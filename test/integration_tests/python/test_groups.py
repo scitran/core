07b18ba605e0df2b494125abe060783557d83814
Harsha Kethineni <harshakethineni@flywheel.io>
2017-05-19 11:35:32 -0500

    Add failing test

--- test/integration_tests/python/test_groups.py
+++ test/integration_tests/python/test_groups.py
@@ -1,3 +1,5 @@
+from dateutil.parser import parse
+
 def test_groups(as_admin, data_builder):
     # Cannot find a non-existant group
     r = as_admin.get('/groups/non-existent')
@@ -8,11 +10,16 @@ def test_groups(as_admin, data_builder):
     # Able to find new group
     r = as_admin.get('/groups/' + group)
     assert r.ok
+    first_modified = r.json()['modified']
 
     # Able to change group name
     group_name = 'New group name'
     r = as_admin.put('/groups/' + group, json={'name': group_name})
     assert r.ok
+    second_modified = r.json()['modified']
+    d1 = parse(first_modified)
+    d2 = parse(second_modified)
+    assert d2 > d1
 
     r = as_admin.get('/groups/' + group)
     assert r.ok
