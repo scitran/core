e6d1d008b3625f52860b9ecb83e926e84e23a8a1
Ambrus Simon <ambrussimon@invenshure.com>
2017-07-11 21:59:46 +0200

    add tests for all dataexplorer handler methods

--- test/unit_tests/python/test_dataexplorer.py
+++ test/unit_tests/python/test_dataexplorer.py
@@ -1,5 +1,11 @@
+import copy
+import json
+
+import api.handlers.dataexplorerhandler as deh
+
+
 def test_search(as_public, as_drone, es):
-    # try to access search w/o login
+    # try to search w/o login
     r = as_public.post('/dataexplorer/search')
     assert r.status_code == 403
 
@@ -25,6 +31,9 @@ def test_search(as_public, as_drone, es):
         {'by_top_hit': {'hits': {'hits': [results]}}},
     ]}}}
     # NOTE maybe sources would read better as module level constants in dataexplorerhandler?
+    # NOTE try: str(x) will never fail (probably not the desired behavior) at
+    # * _parse_request#248
+    # * search_fields#284
     session_sources = [
         'permissions.*', 'session._id', 'session.label', 'session.created', 'session.timestamp', 'subject.code',
         'project.label', 'group.label', 'group._id', 'project._id', 'session.archived', 'project.archived']
@@ -140,3 +149,124 @@ def test_search(as_public, as_drone, es):
         index='data_explorer')
     assert r.status_code == 200
     assert r.json['results'] == results
+
+
+def test_get_facets(as_public, as_drone, es):
+    # try to get facets w/o login
+    r = as_public.post('/dataexplorer/facets')
+    assert r.status_code == 403
+
+    # get facets w/o sending body
+    subject_age = 'test'
+    es.search.return_value = {'aggregations': {
+        'session_age': {'subject.age': subject_age},
+        'by_session': {}}}
+    r = as_drone.post('/dataexplorer/facets')
+    body = copy.deepcopy(deh.FACET_QUERY)
+    body.update({'query': {'match_all': {}}})
+    es.search.assert_called_with(body=body, doc_type='flywheel', index='data_explorer')
+    assert r.status_code == 200
+    assert r.json == {'facets': {'by_session': {'subject.age': subject_age}}}
+
+
+def test_search_fields(as_public, as_drone, es):
+    # try to search fields w/o login
+    r = as_public.post('/dataexplorer/search/fields')
+    assert r.status_code == 403
+
+    # search fields
+    query_field, result_source = 'field', 'source'
+    es.search.return_value = {'hits': {'hits': [{'_source': result_source}]}}
+    r = as_drone.post('/dataexplorer/search/fields', json={'field': query_field})
+    es.search.assert_called_with(
+        body={'size': 20, 'query': {'match': {'name': query_field}}},
+        doc_type='flywheel_field',
+        index='data_explorer_fields')
+    assert r.status_code == 200
+    assert r.json == [result_source]
+
+
+def test_index_fields(as_public, as_drone, es):
+    # try to index fields w/o login
+    r = as_public.post('/dataexplorer/index/fields')
+    assert r.status_code == 403
+
+    # setup es indices mock
+    indices = set()
+    def es_indices_exists(index): return index in indices
+    def es_indices_create(index=None, body=None): indices.add(index)
+    def es_indices_delete(index): indices.remove(index)
+    es.indices.exists.side_effect = es_indices_exists
+
+    # try to index fields before data_explorer index is available
+    r = as_drone.post('/dataexplorer/index/fields')
+    es.indices.exists.assert_called_with('data_explorer')
+    assert r.status_code == 404
+    indices.add('data_explorer')
+
+    # try to (re)index data_explorer_fields w/ hard-reset=true (exc @ delete)
+    indices.add('data_explorer_fields')
+    es.indices.delete.side_effect = Exception('delete')
+    r = as_drone.post('/dataexplorer/index/fields?hard-reset=true')
+    es.indices.delete.assert_called_with(index='data_explorer_fields')
+    assert r.status_code == 500
+    es.indices.delete.side_effect = es_indices_delete
+
+    # try to (re)index data_explorer_fields w/ hard-reset=true (exc @ create)
+    es.indices.create.side_effect = Exception('create')
+    r = as_drone.post('/dataexplorer/index/fields?hard-reset=true')
+    es.indices.exists.assert_called_with('data_explorer_fields')
+    assert es.indices.create.called
+    assert r.status_code == 500
+    es.indices.create.side_effect = es_indices_create
+
+    # try to (re)index data_explorer_fields w/ hard-reset=true (exc @ get_mapping)
+    es.indices.get_mapping.side_effect = Exception('get_mapping')
+    r = as_drone.post('/dataexplorer/index/fields?hard-reset=true')
+    assert r.status_code == 404
+    es.indices.get_mapping.side_effect = None
+
+    # (re)index data_explorer_fields w/ hard-reset=true
+    r = as_drone.post('/dataexplorer/index/fields?hard-reset=true')
+    es.indices.create.assert_called_with(index='data_explorer_fields', body={
+        'settings': {'number_of_shards': 1, 'number_of_replicas': 0, 'analysis': deh.ANALYSIS},
+        'mappings': {'_default_': {'_all': {'enabled' : True}, 'dynamic_templates': deh.DYNAMIC_TEMPLATES}, 'flywheel': {}}})
+    assert r.status_code == 200
+
+    # index data_explorer_fields - test ignored fields
+    ignored_fields = ['_all', 'dynamic_templates', 'analysis_reference', 'file_reference', 'parent', 'container_type', 'origin', 'permissions', '_id']
+    fields = {field: None for field in ignored_fields}
+    es.indices.get_mapping.return_value = {'data_explorer': {'mappings': {'flywheel': {'properties': fields}}}}
+    es.index.reset_mock()
+    r = as_drone.post('/dataexplorer/index/fields')
+    assert not es.indices.index.called
+    assert r.status_code == 200
+
+    # index data_explorer_fields - test type "flattening"
+    type_map = {
+        'string':  ['text', 'keyword'],
+        'integer': ['long', 'integer', 'short', 'byte'],
+        'float':   ['double', 'float'],
+        'date':    ['date'],
+        'boolean': ['boolean'],
+        'object':  ['object'],
+        None:      ['unrecognized'],
+        # NOTE _get_field_type returns None for unrecognized field_types
+    }
+    type_map_r = {vi: k for k, v in type_map.iteritems() for vi in v}
+    fields = {k + 'field': {'type': k} for k in type_map_r}
+    es.indices.get_mapping.return_value = {'data_explorer': {'mappings': {'flywheel': {'properties': fields}}}}
+    es.index.reset_mock()
+    r = as_drone.post('/dataexplorer/index/fields')
+    for field_name in fields:
+        field_type = type_map_r[field_name.replace('field', '')]
+        if field_type == 'object':
+            continue
+        es.index.assert_any_call(
+            body=json.dumps({'name': field_name, 'type': field_type}),
+            doc_type='flywheel_field',
+            id=field_name,
+            index='data_explorer_fields')
+    assert r.status_code == 200
+
+    # TODO index data_explorer_fields - test recursion
