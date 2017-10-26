b043b861d8d36f6f85a17240f4d0bbe80c5d5ad7
David Farkas <david.farkas93@gmail.com>
2017-10-26 03:52:01 +0200

    Add some documentation, and increase code coverage

--- api/handlers/filetypehandler.py
+++ api/handlers/filetypehandler.py
@@ -8,13 +8,13 @@ class FileType(base.RequestHandler):
 
     def get(self):
         """Get file types"""
-        resp = config.db.filetypes.find()
-        if resp != None:
-            return resp
-        else:
-            self.abort(404, "Version document does not exist")
+        return config.db.filetypes.find()
 
     def post(self):
+        """
+        Insert or replace a file type. Required fields: '_id' and 'regex' where the '_id' is the unique name of
+        the file type and 'regex' is a regular expression which is used to figure out the file type from the file name.
+        """
         permchecker = userauth.default(self)
         payload = self.request.json_body
         mongo_schema_uri = validators.schema_uri('mongo', 'filetype.json')
@@ -28,10 +28,11 @@ class FileType(base.RequestHandler):
             self.abort(404, 'File type {} not updated'.format(payload['_id']))
 
     def delete(self, _id):
+        """Delete a file type"""
         permchecker = userauth.default(self)
         permchecker(noop)('DELETE', _id)
         result = config.db.filetypes.delete_one({'_id': _id})
-        if result.acknowledged:
+        if result.deleted_count:
             return {'deleted': result.deleted_count}
         else:
             self.abort(404, 'File type {} not removed'.format(_id))
--- tests/integration_tests/python/test_files.py
+++ tests/integration_tests/python/test_files.py
@@ -1,6 +1,5 @@
 from api import files
 
-
 def test_extension():
     assert files.guess_type_from_filename('example.pdf') == 'pdf'
 
@@ -22,9 +21,19 @@ def test_qa():
 def test_unknown():
     assert files.guess_type_from_filename('example.unknown') == None
 
-def test_insert_delete(as_drone):
-    as_drone.post('/filetype', json={'_id': 'new', 'regex': '.*\.new$'})
+def test_get_insert_delete(as_drone):
+    r = as_drone.get('/filetype')
+    assert r.ok
+    r = as_drone.post('/filetype', json={'_id': 'new', 'regex': '.*\.new$'})
+    assert r.ok
     assert files.guess_type_from_filename('example.new') == 'new'
-    as_drone.post('/filetype', json={'_id': 'new', 'regex': '.*\.new2$'})
+    r = as_drone.post('/filetype', json={'_id': 'new', 'regex': '.*\.new2$'})
+    assert r.ok
     assert files.guess_type_from_filename('example.new') == None
     assert files.guess_type_from_filename('example.new2') == 'new'
+    r = as_drone.delete('/filetype/new')
+    assert r.ok
+
+def test_insert_delete_abort(as_drone):
+    r = as_drone.delete('/filetype/notexists')
+    assert r.status_code == 404
