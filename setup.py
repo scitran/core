5dd03e902474ea39170f290a90c2b77eba4541dd
Ambrus Simon <ambrussimon@invenshure.com>
2017-11-17 18:30:45 +0100

    Requirements overhaul

--- setup.py
+++ setup.py
@@ -1,23 +1,7 @@
 from setuptools import setup, find_packages
 
-install_requires = [
-    'django >= 1.11.5',
-    'elasticsearch==5.3.0',
-    'enum34==1.1.6',
-    'jsonschema==2.6.0',
-    'Markdown==2.6.5',
-    'pymongo==3.2',
-    'python-dateutil==2.4.2',
-    'pytz==2015.7',
-    'requests==2.9.1',
-    'rfc3987==1.3.4',
-    'strict-rfc3339==0.7',
-    'unicodecsv==0.9.0',
-    'webapp2==2.5.2',
-    'WebOb==1.5.1',
-    'gears',
-]
-
+install_requires = open('requirements.txt').readlines()
+tests_require = open('tests/requirements.txt').readlines()
 dependency_links = [
     'git+https://github.com/flywheel-io/gears.git@v0.1.4#egg=gears',
 ]
@@ -33,4 +17,5 @@ setup(
     packages = find_packages(),
     dependency_links = dependency_links,
     install_requires = install_requires,
+    tests_require = tests_require,
 )
