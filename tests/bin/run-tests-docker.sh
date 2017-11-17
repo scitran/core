d1c32bbf76e781ea71a66760b7cbb409d5f19377
Ambrus Simon <ambrussimon@invenshure.com>
2017-11-17 19:12:55 +0100

    WIP - test script reorg

--- tests/bin/run-tests-docker.sh
+++ tests/bin/run-tests-docker.sh
@@ -45,20 +45,27 @@ main() {
 
     docker network create scitran-core-test-network
 
-    # Launch Mongo instance
+    # Launch core + mongo
+    local SCITRAN_CORE_DRONE_SECRET=T+27oHSKw+WQqT/rre+iaiIY4vNzav/fPStHqW/Eczk=
+
+    set -x
     docker run -d \
-        --name scitran-core-test-mongo \
+        --name scitran-core-test-service \
         --network scitran-core-test-network \
-        mongo:${MONGO_VERSION}
+        --volume $(pwd):/var/scitran/code/api \
+        --env SCITRAN_CORE_DRONE_SECRET=$SCITRAN_CORE_DRONE_SECRET \
+        --env SCITRAN_RUNTIME_COVERAGE=true \
+        --env SCITRAN_CORE_ACCESS_LOG_ENABLED=true \
+        scitran-core:run-tests
 
     # Execute tests
     docker run -it \
-        --name scitran-core-test-uwsgi \
+        --name scitran-core-test-runner \
         --network scitran-core-test-network \
-        -e SCITRAN_PERSISTENT_DB_URI=mongodb://scitran-core-test-mongo:27017/scitran \
-        -e SCITRAN_PERSISTENT_DB_LOG_URI=mongodb://scitran-core-test-mongo:27017/logs \
-        -v $(pwd):/var/scitran/code/api \
-        --entrypoint sh \
+        --volume $(pwd):/var/scitran/code/api \
+        --env SCITRAN_CORE_DRONE_SECRET=$SCITRAN_CORE_DRONE_SECRET \
+        --env SCITRAN_PERSISTENT_DB_URI=mongodb://scitran-core-test-service:27017/scitran \
+        --env SCITRAN_PERSISTENT_DB_LOG_URI=mongodb://scitran-core-test-service:27017/logs \
         scitran-core:run-tests \
         /var/scitran/code/api/tests/bin/run-tests-ubuntu.sh \
         $TEST_ARGS
@@ -70,11 +77,10 @@ clean_up() {
     set +e
 
     # Copy coverage file to host for possible further reporting
-    docker cp scitran-core-test-uwsgi:/var/scitran/code/api/.coverage .coverage
+    docker cp scitran-core-test-service:/var/scitran/code/api/.coverage .coverage
 
     # Spin down dependencies
-    docker rm -f -v scitran-core-test-uwsgi
-    docker rm -f -v scitran-core-test-mongo
+    docker rm -f -v scitran-core-test-service
     docker network rm scitran-core-test-network
     exit $TEST_RESULT_CODE
 }
