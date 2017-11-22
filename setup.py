d2fac9c301b3a80c0bb537d97eb5498246991d73
Gunnar Schaefer <gsfr@flywheel.io>
2017-11-21 17:40:40 -0800

    Add author and maintainer info

--- setup.py
+++ setup.py
@@ -6,13 +6,15 @@ dependency_links = [r for r in requirements if r.startswith('git+')]
 tests_require = open('tests/requirements.txt').readlines()
 
 setup(
-    name = 'api',
-    version = '0.0.1',
-    description = '',
-    author = '',
-    author_email = '',
-    url = '',
-    license = '',
+    name = 'core',
+    version = '1.0.0',
+    description = 'Scitran API Core',
+    author = 'Gunnar Schaefer',
+    author_email = 'gsfr@flywheel.io',
+    maintainer = 'Megan Henning, Ambrus Simon',
+    maintainer_email = 'meganhenning@flywheel.io, ambrussimon@invenshure.com',
+    url = 'https://github.com/scitran/core',
+    license = 'MIT',
     packages = find_packages(),
     dependency_links = dependency_links,
     install_requires = install_requires,
