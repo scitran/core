b0550a3582afb9e3c45359d32fdb02f810ac5fd1
Harsha Kethineni <harshakethineni@flywheel.io>
2017-11-27 11:31:33 -0600

    Tested Key Error handling

--- tests/integration_tests/python/test_errors.py
+++ tests/integration_tests/python/test_errors.py
@@ -13,7 +13,7 @@ def test_extra_param(as_admin):
     assert r.ok
     assert not any(project['label'] == label for project in r.json())
 
-def test_error_response(as_admin, as_user, as_public, data_builder):
+def test_error_response(as_admin, as_user, as_public, as_root, data_builder, api_db):
 
     group = data_builder.create_group()
     project = data_builder.create_project()
@@ -32,3 +32,10 @@ def test_error_response(as_admin, as_user, as_public, data_builder):
     r = as_user.put('/projects/' + project, json={'label':'Project'})
     assert r.status_code == 403
     assert r.json().get('request_id')
+
+    # Test Key Error
+    analysis = api_db.analyses.insert_one({'label':'no-parent'}).inserted_id
+    r = as_root.get('/analyses/' + str(analysis))
+    assert r.status_code == 500
+    assert r.json().get('message') == "Key 'parent' was not found"
+    api_db.analyses.delete_one({'_id': analysis})
