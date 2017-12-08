0499134c8ded0f387b0ce5494661b94f7e7ea681
Justin Ehlert <justinehlert@flywheel.io>
2017-12-08 10:33:27 -0600

    Configured livereload for swagger-ui

--- swagger/Gruntfile.js
+++ swagger/Gruntfile.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var loadTasks = require('load-grunt-tasks');
+var SWAGGER_UI_PORT = 9009;
+var SWAGGER_UI_LIVE_RELOAD_PORT = 19009;
 
 module.exports = function(grunt) {
 	require('load-grunt-tasks')(grunt);
@@ -9,15 +11,51 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		
-		/**
-		 * Copy schema files into build. Once we fully transition to swagger,
-		 * the schema files should live in this subdirectory permanently.
-		 */
 		copy: {
+			/**
+			 * Copy schema files into build. Once we fully transition to swagger,
+			 * the schema files should live in this subdirectory permanently.
+			 */
 			schema: {
 				files: [
-					{ expand: true, cwd: '../raml/schemas', src: ['**'], dest: 'build/schemas' }
+					{ 
+						expand: true, 
+						cwd: '../raml/schemas', 
+						src: ['**'], 
+						dest: 'build/schemas' 
+					}
 				]
+			},
+			/**
+			 * Copy swagger ui dist and config files
+			 */
+			swaggerUi: {
+				files: [
+					{
+						expand: true,
+						cwd: 'swagger-ui',
+						src: ['**'],
+						dest: 'build/swagger-ui'
+					},
+					{
+						expand: true, 
+						cwd: './node_modules/swagger-ui-dist', 
+						src: [
+							'swagger-ui-bundle.js',
+							'swagger-ui-standalone-preset.js',
+							'*.png',
+							'*.css'
+						],
+						dest: 'build/swagger-ui'
+					}
+				]
+			},
+			/**
+			 * Copy swagger to swagger-ui
+			 */
+			swaggerUiSchema: {
+				src: 'build/swagger-flat.json',
+				dest: 'build/swagger-ui/swagger.json'
 			}
 		},
 
@@ -30,10 +68,65 @@ module.exports = function(grunt) {
 				apiFile: 'index.yaml',
 				dest: 'build/swagger-flat.json'
 			}
+		},
+
+		/**
+		 * Static hosting for swagger-ui docs
+		 */
+		connect: {
+			uiServer: {
+				options: {
+					port: SWAGGER_UI_PORT,
+					base: 'build/swagger-ui',
+					livereload: SWAGGER_UI_LIVE_RELOAD_PORT,
+					open: true
+				}
+			}
+		},
+
+		/**
+		 * Live reload for swagger-ui
+		 */
+		watch: {
+			apis: {
+				options: {
+					livereload: SWAGGER_UI_LIVE_RELOAD_PORT
+				},
+				files: [
+					'**/*.yaml',
+					'../raml/schemas/**/*.json'
+				],
+				tasks: [
+					'build-schema',
+					'copy:swaggerUiSchema'
+				]
+			}
 		}
 	});
 
-	grunt.registerTask('default', ['copy', 'flattenSwagger']);
+	/**
+	 * Build the swagger schemas
+	 */
+	grunt.registerTask('build-schema', [
+		'copy:schema', 
+		'flattenSwagger'
+	]);
+
+	/**
+	 * Build swagger-ui
+	 */
+	grunt.registerTask('build-ui', [
+		'build-schema',
+		'copy:swaggerUi',
+		'copy:swaggerUiSchema'
+	]);
+
+	grunt.registerTask('default', ['build-ui']);
+
+	/**
+	 * Run a live server with swagger-ui
+	 */
+	grunt.registerTask('live', ['build-ui', 'connect', 'watch']);
 };
 
 
