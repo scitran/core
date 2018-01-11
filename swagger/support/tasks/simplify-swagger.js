69cbe5c94602f9bcfa35e8b165478ff4a08bd511
Justin Ehlert <justinehlert@flywheel.io>
2018-01-11 16:10:53 -0600

    Replace pure references in definitions with aliases

--- swagger/support/tasks/simplify-swagger.js
+++ swagger/support/tasks/simplify-swagger.js
@@ -106,6 +106,10 @@ module.exports = function(grunt) {
 				} else {
 					grunt.log.writeln('ERROR '.red + 'Cannot find alias for: ' + path + ' (' + schema.allOf[0].$ref + ')');
 				}
+			} else if( schema.$ref ) {
+				// Replace pure references
+				context.aliases[path] = schema;
+				delete defs[k];
 			} else if( Schemas.isPrimitiveType(schema.type) ) {
 				// For simple types in definitions, alias them
 				context.aliases[path] = schema;
