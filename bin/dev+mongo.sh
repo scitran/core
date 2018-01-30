bb73a8e5834980e231556f264954ea29863bb488
Gunnar Schaefer <gsfr@flywheel.io>
2018-01-30 12:19:59 -0800

    Only chown if needed

--- bin/dev+mongo.sh
+++ bin/dev+mongo.sh
@@ -3,6 +3,8 @@
 mongod &
 MONGOD_PID=$!
 
-chown nobody:nobody -R $SCITRAN_PERSISTENT_DATA_PATH
+if [ "$(stat -c %U:%G $SCITRAN_PERSISTENT_DATA_PATH)" != "nobody:nobody" ]; then
+    chown nobody:nobody -R $SCITRAN_PERSISTENT_DATA_PATH
+fi
 
 exec unitd --control "*:8080" --no-daemon --log /dev/stdout
