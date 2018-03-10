fe5dbfc22e31dd22dd7ce896cb9077d175722efe
Gunnar Schaefer <gsfr@flywheel.io>
2018-03-09 16:56:34 -0800

    Add upgrade_schema to dev

--- bin/dev+mongo.sh
+++ bin/dev+mongo.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+unset CDPATH
+cd "$(dirname "$0")"
+
 nginx
 
 mongod &
@@ -7,5 +10,7 @@ mongod &
 if [ "$(stat -c %U:%G $SCITRAN_PERSISTENT_DATA_PATH)" != "nobody:nobody" ]; then
     chown nobody:nobody -R $SCITRAN_PERSISTENT_DATA_PATH
 fi
+
+./db_upgrade.py upgrade_schema
 
 exec unitd --control "*:8088" --no-daemon --log /dev/stdout
